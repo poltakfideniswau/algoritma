b2d1e46473297376034c33a143cf4aeec630ae95
poltakfideniswau <wauden3@gmail.com>
2025-05-02 19:56:52 +0700

    Add files via upload

---
+++ poltakc++.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int umur;
+	cout<<"Masukan Umur : ";
+	cin>>umur;
+	
+	if(umur<=18){
+		if(umur<=10){
+			cout<<"Anak-anak"<<endl;
+		}else{
+			cout<<"Remaja"<<endl;
+		}
+		
+	}else{
+		cout<<"Bukan usia anak maupun remaja"<<endl;
+	}
+}
+	
+	
